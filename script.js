30eb4760b5250c5117c8b25ea1884b3e0bc5aefc
AlissCava <cavalleraalissa@gmail.com>
2023-11-27 12:24:41 +0100

    fineeeeee

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var contatore = 0;
 
 // Ciclo while per aggiungere gli elementi alla lista ordinata
 while (contatore < listaSpesa.length) {
+    
     // Creiamo un elemento <li> per ciascun elemento della lista
     var listItem = document.createElement("li");
 
